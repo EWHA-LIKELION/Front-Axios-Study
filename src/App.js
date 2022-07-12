e9da392e76c8d7ef0ec98da20794135487f9d9f1
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-12 23:33:40 +0900

    feat : get 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [loading, isLoading] = useState(false);
+
+  const getPosts = async () => {
+    const response = await axios
+      .get("https://dy6578.pythonanywhere.com/api/posts")
+      .then(response => {
+        setPosts([...response.data]);
+      })
+      .catch(error => {
+        console.log("전체 글 오류", error.message);
+      });
+  };
+
+  const getSinglePost = async () => {
+    isLoading(true);
+    const response = await axios
+      .get("https://dy6578.pythonanywhere.com/api/posts/6")
+      .then(response => {
+        console.log(response.data);
+        setPost(response.data);
+        isLoading(false);
+      })
+      .catch(error => {
+        console.log("글 하나 오류", error);
+      });
+  };
+
+  useEffect(() => {
+    getPosts();
+    getSinglePost();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map(post => {
+        return <p style={{ border: "1px solid red" }}>{post.content}</p>;
+      })}
+
+      <h1>{post.content}</h1>
+      <p>댓글 : {post.comment[0].content}</p>
     </div>
   );
 }
